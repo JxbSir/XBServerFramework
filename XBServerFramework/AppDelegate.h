ff4e47d22d800f5290e6852ce4f77769f86a354e
king824 <i@jxb.name>
2015-04-15 13:11:20 +0800

    Initial Commit

---
+++ XBServerFramework/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XBServerFramework
+//
+//  Created by Peter on 15/4/15.
+//  Copyright (c) 2015年 Peter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
